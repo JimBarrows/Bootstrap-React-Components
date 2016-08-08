330d067f2e819f297465798f96d8c69616a10040
Jim Barrows <jim@reallybigtree.com>
2016-08-08 07:24:10 -0700

    Editor works.

--- src/client.js
+++ src/client.js
@@ -5,13 +5,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import BasicEditor from "./BasicEditor.jsx";
 import PageHeader from "./PageHeader.jsx";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.min.css";
+import TextAreaFormGroup from "./TextAreaFormGroup.jsx";
 
 const app = document.getElementById('app');
 ReactDOM.render(
 		<div>
 			<PageHeader title="Showcase"/>
-			<BasicEditor/>
+			<TextAreaFormGroup label="Text area" name="textARea" error="" value="The quick brown fox."/>
 		</div>, app);
