f2263e71eb15ac723d49d2ca4671b2b51874fb37
Jim Barrows <jim@reallybigtree.com>
2016-08-19 09:36:17 -0700

    Fixed the build process so that it will build an ES5 compatible library.

---
+++ src/index.jsx
@@ -0,0 +1,49 @@
+/**
+ * Created by JimBarrows on 8/10/16.
+ */
+'use strict';
+
+import AddButton from "./AddButton";
+import BasicEditor from "./BasicEditor";
+import ButtonGroup from "./ButtonGroup";
+import CheckboxFormGroup from "./CheckboxFormGroup";
+import DangerAlert from "./DangerAlert";
+import DatePickerFormGroup from "./DatePickerFormGroup";
+import DateRangFormGroup from "./DateRangeFormGroup";
+import FormGroup from "./FormGroup";
+import ListPanel from "./ListPanel";
+import ListTablePanel from "./ListTablePanel";
+import PageHeader from "./PageHeader";
+import Panel from "./Panel";
+import PanelBody from "./PanelBody";
+import PanelHeader from "./PanelHeader";
+import PasswordFormGroup from "./PasswordFormGroup";
+import ReloadButton from "./ReloadButton";
+import SelectFormGroup from "./SelectFormGroup";
+import StripedTable from "./StripedTable";
+import TextAreaFormGroup from "./TextAreaFormGroup";
+import TextFormGroup from "./TextFormGroup";
+
+module.exports = {
+	AddButton,
+	BasicEditor,
+	ButtonGroup,
+	CheckboxFormGroup,
+	DangerAlert,
+	DatePickerFormGroup,
+	DateRangFormGroup,
+	FormGroup,
+	ListPanel,
+	ListTablePanel,
+	PageHeader,
+	Panel,
+	PanelBody,
+	PanelHeader,
+	PasswordFormGroup,
+	ReloadButton,
+	SelectFormGroup,
+	StripedTable,
+	TablePanel,
+	TextAreaFormGroup,
+	TextFormGroup
+};
