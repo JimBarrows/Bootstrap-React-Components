b2ed9ac352ccef0ba4260eba8a4983ce2642624a
Jim Barrows <jim@reallybigtree.com>
2016-08-20 09:31:29 -0700

    Forget optional, just provide the freaking id.

--- src/DangerAlert.jsx
+++ src/DangerAlert.jsx
@@ -4,13 +4,10 @@ export default class DangerAlert extends React.Component {
 
 	render() {
 		const {error, id}      = this.props;
-		let optionalAttributes = [];
-		if (id) {
-			optionalAttributes.push({id});
-		}
+
 		var Alert = null;
 		if (error) {
-			Alert = (<div {...optionalAttributes} class="alert alert-danger" role="alert">{error}</div>)
+			Alert = (<div id={id} class="alert alert-danger" role="alert">{error}</div>)
 		}
 		return Alert
 
