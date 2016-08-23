a9eac9c3d266de05e2c3907c75344362956dec0d
Jim Barrows <jim@reallybigtree.com>
2016-08-22 17:31:22 -0700

    The add button was using the name for some reason.  Removed name, and made it id specific.

--- src/ListPanel.jsx
+++ src/ListPanel.jsx
@@ -9,14 +9,13 @@ import AddButton from "./AddButton";
 export default class ListPanel extends React.Component {
 
 	render() {
-		let {id, name, onAddClick, title} = this.props;
-		let addButtonId                   = name + id + "AddButton";
+		let {id, onAddClick, title} = this.props;
 		return (
-				<Panel id={id }>
+				<Panel id={id}>
 					<PanelHeader id={id}>
 						<div class="panel-title pull-left">{title}</div>
 						<div class="btn-group pull-right">
-							<AddButton id={addButtonId} onClick={onAddClick}/>
+							<AddButton id={id + "AddButton"} onClick={onAddClick}/>
 						</div>
 					</PanelHeader>
 					<PanelBody id={id}>
