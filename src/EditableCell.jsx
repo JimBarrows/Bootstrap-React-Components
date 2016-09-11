7d260852f5847e703fdd30a5e5b60b4966109427
Jim Barrows <jim@reallybigtree.com>
2016-09-11 12:37:53 -0700

    Cleanup of editable cell.

--- src/EditableCell.jsx
+++ src/EditableCell.jsx
@@ -18,9 +18,9 @@ export default class EditableCell extends React.Component {
 
 	render() {
 		let {id, onChange, type, value}     = this.props;
-		let {edit}                          = this.state.edit;
 		let input                           = this.state.edit ?
-				<input id={id} type={type} defaultValue={value} onChange={onChange}/> : <span id={id + "Value"}>{value}</span>;
+				<input id={id} type={type} defaultValue={value} onChange={onChange}/> :
+				<span id={id + "Value"}>{value}</span>;
 		return (<td id={id + "Cell"}>{input}</td>);
 	}
 }
