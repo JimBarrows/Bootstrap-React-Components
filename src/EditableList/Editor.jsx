43bcf9ca71aee173d1cdc3a64c5d26b5c6018604
Jim Barrows <jim.barrows@gmail.com>
2017-12-16 09:34:03 -0600

    Linted the list stuff

--- src/EditableList/Editor.jsx
+++ src/EditableList/Editor.jsx
@@ -1,60 +1,77 @@
-import React from "react";
+import PropTypes from 'prop-types'
+import React from 'react'
 
 class Editor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      current: props.item
+    }
+    this.onCancel = this.onCancel.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
 
-	componentWillReceiveProps(nextProps) {
-		let { item } = nextProps;
-		this.setState({
-			current: item
-		})
-	}
+  componentWillReceiveProps (nextProps) {
+    let {item} = nextProps
+    this.setState(
+      {
+        current: item
+      }
+    )
+  }
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			current: props.item
-		}
-	}
+  onCancel (e) {
+    e.preventDefault()
+    this.setState(
+      {
+        current: this.props.item
+      }
+    )
+    if (this.props.onCancel) {
+      this.props.onCancel(e)
+    }
+  }
 
-	onCancel( e) {
-		e.preventDefault();
-		this.setState({
-			current: this.props.item
-		})
-		if( this.props.onCancel) {
-			this.props.onCancel( e);
-		}
-	}
+  onChange (e) {
+    this.setState(
+      {
+        current: this.props.onChange(e, this.state.current)
+      }
+    )
+  }
 
-	onChange(e) {
-		this.setState({
-			current: this.props.onChange(e, this.state.current)
-		});
-	}
+  onSubmit (e) {
+    e.preventDefault()
+    this.props.onSubmit(this.state.current)
+  }
 
-	onSubmit( e ) {
-		e.preventDefault( );
-		this.props.onSubmit( this.state.current );
-	}
+  render () {
+    let {id, formElements} = this.props
 
-	render( ) {
-		let { id, formElements } = this.props;
+    let childrenWithProps = React.Children.map(formElements(this.state.current),
+                                               (child) => React.cloneElement(child, {
+                                                 onChange: this.onChange
+                                               })
+    )
 
-		let childrenWithProps = React.Children.map(formElements(this.state.current),
-     (child) => React.cloneElement(child, {
-       onChange: this.onChange.bind(this)
-     })
-    );
-
-		return (
-			<form id="defaultForm" onSubmit={this.onSubmit.bind( this )}>
-				{childrenWithProps}
-				<button id={"submit_form_" + id} type="submit" class="btn btn-success">Save</button>
-				<button id={"cancel_form_" + id} onClick={this.onCancel.bind(this)} class="btn btn-default" type="button">Cancel</button>
-			</form>
-		);
-	}
-
+    return (
+      <form id='defaultForm' onSubmit={this.onSubmit} >
+        {childrenWithProps}
+        <button id={'submit_form_' + id} type='submit' className='btn btn-success' >Save</button >
+        <button id={'cancel_form_' + id} onClick={this.onCancel} className='btn btn-default' type='button' >Cancel
+        </button >
+      </form >
+    )
+  }
 }
 
-export default Editor;
+Editor.propTypes = {
+  formElements: PropTypes.node.isRequired,
+  id: PropTypes.string.isRequired,
+  item: PropTypes.object.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
+
+export default Editor
