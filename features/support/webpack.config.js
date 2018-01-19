d12504c0e5ac4e326e2f6122e71ec92e253fc78c
Jim Barrows <Jim.Barrows@gmail.com>
2018-01-19 09:32:36 -0600

    Updated all the packages to their latest except for bootstrap

--- features/support/webpack.config.js
+++ features/support/webpack.config.js
@@ -1,62 +1,62 @@
-'use strict'
-
 var path = require('path')
 var webpack = require('webpack')
 
-var definePlugin = new webpack.DefinePlugin({
+var definePlugin = new webpack.DefinePlugin(
+  {
     __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
     __PRERELEASE__: JSON.stringify(JSON.parse(process.env.BUILD_PRERELEASE || 'false'))
-});
+  }
+)
+
 module.exports = {
   devtool: 'eval-source-map',
-    entry: {
-      main: './features/support/main.jsx'
-    },
-    output: {
-      path: './features/support/html/js',
-      filename: '[name].js'
-    },
-    plugins: [
-        definePlugin
-    ],
-    module: {
-        loaders: [{
-                test: /\.jsx?$/,
-          loader: 'babel',
-                exclude: /(node_modules|bower_components)/,
-                query: {
-                  // presets: ['react', 'es2015', 'stage-2', 'react-html-attrs'],
-                  // plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-                }
-            },
-            {
-                test: /\.js?$/,
-              loader: 'babel',
-                exclude: /(node_modules|bower_components)/,
-                query: {
-                  // presets: ['react', 'es2015', 'stage-2'],
-                  // plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-                }
-            }, {
-                test: /\.css$/,
-            loader: 'style!css'
-            }, {
-                test: /\.(ttf|eot|svg|woff(2)?)(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url'
-            }, {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-            loader: 'url?limit=10000&mimetype=image/svg+xml'
-            }, {
-                test: /\.json$/,
-            loader: 'json-loader'
-            }, {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-            loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap&includePaths[]=node_modules/compass-mixins/lib'
-            }
-        ]
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
+  entry: {
+    main: path.resolve(__dirname, 'main.jsx')
+  },
+  output: {
+    path: path.resolve(__dirname, 'html/js'),
+    filename: '[name].js'
+  },
+  plugins: [
+    definePlugin
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /(node_modules|bower_components)/,
+      query: {
+        // presets: ['react', 'es2015', 'stage-2', 'react-html-attrs'],
+        // plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+      }
+    }, {
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: /(node_modules|bower_components)/,
+      query: {
+        // presets: ['react', 'es2015', 'stage-2'],
+        // plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.(ttf|eot|svg|woff(2)?)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader'
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      loader: 'style-loader!css-loader?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap&includePaths[]=node_modules/compass-mixins/lib'
     }
-};
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
