231bd9ea07dfd7cce9234157a0e7549cab8b95e2
Jim Barrows <Jim.Barrows@gmail.com>
2018-02-22 07:29:17 -0600

    Fixes #83 Refactored to try and get the import for css working correctly, but it doesn't.

--- features/support/SupportedControls.jsx
+++ features/support/SupportedControls.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {ColorFormGroup, PasswordFormGroup, RangeFormGroup, TextAreaFormGroup} from '../../src'
+import TextFormGroup from '../../src/TextFormGroup'
 
 export default class SupportedControls extends React.Component {
   constructor (props) {
@@ -8,31 +9,39 @@ export default class SupportedControls extends React.Component {
       passwordFormGroupValue: '',
       colorFormGroupValue: '',
       rangeFormGroupValue: '',
-      textAreaFormGroupValue: ''
+      textAreaFormGroupValue: '',
+      textFormGroupValue: ''
     }
   }
 
   render () {
     return (
       <div id={'SupportedControlsComponent'} >
-        <ColorFormGroup id={'colorFormGroupTest'} label='Color Form Group'
-          onChange={e => this.setState({colorFormGroupValue: e.target.value})}
-          value={this.state.colorFormGroupValue} />
-        <p >Color Form Group value: <span id={'colorFormGroupValue'} >{this.state.colorFormGroupValue}</span ></p >
-        <RangeFormGroup id={'rangeFormGroupTest'} label={'Range Form Group'}
-          onChange={e => this.setState({rangeFormGroupValue: e.target.value})}
-          value={this.state.rangeFormGroupValue} />
-        <p >Range Form Group value: <span id={'rangeFormGroupValue'} >{this.state.rangeFormGroupValue}</span ></p >
-        <h4 >Password</h4 >
-        <PasswordFormGroup id={'passwordFormGroupTest'}
-          onChange={e => this.setState({passwordFormGroupValue: e.target.value})}
-          value={this.state.passwordFormGroupValue} />
-        <p >Password value: <span id={'passwordValue'} >{this.state.passwordFormGroupValue}</span ></p >
-        <TextAreaFormGroup id='textAreaFormGroupTest' label='Text Area Form Group'
-          onChange={e => this.setState({textAreaFormGroupValue: e.target.value})}
-          value={this.state.textAreaFormGroupValue} />
-        <p >Text Area Form Group value: <span id={'textAreaFormGroupValue'} >{this.state.textAreaFormGroupValue}</span >
-        </p >
+        <form >
+          <ColorFormGroup id={'colorFormGroupTest'} label='Color Form Group'
+            onChange={e => this.setState({colorFormGroupValue: e.target.value})}
+            value={this.state.colorFormGroupValue} />
+          <p >Color Form Group value: <span id={'colorFormGroupValue'} >{this.state.colorFormGroupValue}</span ></p >
+          <RangeFormGroup id={'rangeFormGroupTest'} label={'Range Form Group'}
+            onChange={e => this.setState({rangeFormGroupValue: e.target.value})}
+            value={this.state.rangeFormGroupValue} />
+          <p >Range Form Group value: <span id={'rangeFormGroupValue'} >{this.state.rangeFormGroupValue}</span ></p >
+          <PasswordFormGroup id={'passwordFormGroupTest'} label={'Password From Group'}
+            onChange={e => this.setState({passwordFormGroupValue: e.target.value})}
+            value={this.state.passwordFormGroupValue} />
+          <p >Password value: <span id={'passwordValue'} >{this.state.passwordFormGroupValue}</span ></p >
+          
+          <TextFormGroup id={'textFormGroupTest'} label={'Text form Group'}
+            onChange={e => this.setState({textFormGroupValue: e.target.value})}
+            required
+            value={this.state.textFormGroupValue} />
+          <TextAreaFormGroup id='textAreaFormGroupTest' label='Text Area Form Group'
+            onChange={e => this.setState({textAreaFormGroupValue: e.target.value})}
+            value={this.state.textAreaFormGroupValue} />
+          <p >Text Area Form Group value: <span
+            id={'textAreaFormGroupValue'} >{this.state.textAreaFormGroupValue}</span >
+          </p >
+        </form >
       </div >
     )
   }
