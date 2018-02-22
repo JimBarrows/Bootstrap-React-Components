721a726da7facb4eefd5dfb0f83eabcb5924fadb
Jim Barrows <Jim.Barrows@gmail.com>
2018-02-22 08:33:25 -0600

    Fixes #86

--- src/RangeFormGroup.jsx
+++ src/RangeFormGroup.jsx
@@ -4,15 +4,18 @@ import FormGroup from './FormGroup'
 
 export default class RangeFormGroup extends React.Component {
   render () {
-    let {disabled, error, id, label, onChange, placeholder, required, value} = this.props
+    let {disabled, error, id, label, max, min, onChange, placeholder, required, step, value} = this.props
     return (
       <FormGroup label={label} id={id} error={error} required={required} >
         <input className='form-control'
           disabled={disabled}
           id={id}
+          max={max}
+          min={min}
           onChange={onChange}
           placeholder={placeholder}
           required={required}
+          step={step}
           type='range'
           value={value} />
       </FormGroup >
@@ -22,7 +25,10 @@ export default class RangeFormGroup extends React.Component {
 
 RangeFormGroup.defaultProps = {
   disabled: false,
-  required: false
+  max: 100,
+  min: 0,
+  required: false,
+  step: 1
 }
 
 RangeFormGroup.propTypes = {
@@ -30,8 +36,11 @@ RangeFormGroup.propTypes = {
   error: PropTypes.string,
   id: PropTypes.string.isRequired,
   label: PropTypes.string,
+  max: PropTypes.number,
+  min: PropTypes.number,
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string,
   required: PropTypes.bool,
+  step: PropTypes.number,
   value: PropTypes.string.isRequired
 }
