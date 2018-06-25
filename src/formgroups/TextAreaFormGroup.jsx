994c6e773ba4a38e7fec2cf26c6fedcf70796455
Jim Barrows <JIm.Barrows@gmail.com>
2018-06-24 19:02:13 -0500

    Text area works

--- src/formgroups/TextAreaFormGroup.jsx
+++ src/formgroups/TextAreaFormGroup.jsx
@@ -1,25 +1,38 @@
-import {FormGroup} from 'bootstrap-react-components'
+import FormGroup from '../bootstrap/forms/FormGroup'
 import PropTypes from 'prop-types'
 import React from 'react'
 import RichTextEditor, {createValueFromString} from 'react-rte'
 
 export default class TextAreaFormGroup extends React.Component {
-  constructor (props) {
+  static defaultProps = {}
+
+  static propTypes = {
+    disabled         : PropTypes.bool,
+    id               : PropTypes.string.isRequired,
+    label            : PropTypes.string,
+    onChange         : PropTypes.func.isRequired,
+    required         : PropTypes.bool,
+    value            : PropTypes.string.isRequired,
+    valid            : PropTypes.bool,
+    validationMessage: PropTypes.string
+  }
+
+  constructor(props) {
     super(props)
-    this.state = {
+    this.state    = {
       value: createValueFromString(props.value, 'markdown')
     }
     this.onChange = this.onChange.bind(this)
   }
 
-  onChange (value) {
+  onChange(value) {
     this.setState({value})
     if (this.props.onChange) {
       this.props.onChange(
         {
           target:
             {
-              id: this.props.id,
+              id   : this.props.id,
               value: value.toString('markdown')
             }
         }
@@ -27,33 +40,23 @@ export default class TextAreaFormGroup extends React.Component {
     }
   }
 
-  render () {
-    let {disabled, error, id, label, placeholder, required} = this.props
-    return <FormGroup label={label} id={id} error={error} required={required} >
+  render() {
+    let {disabled, id, label, onChange, required, value, valid, validationMessage} = this.props
+    let className                                                                  = 'form-control'
+    if( validationMessage) {
+      className += valid ? " is-valid" : " is-invalid"
+    }
+    return <FormGroup id={'TextArea-' + id} label={label} required={required} valid={valid}
+                      validationMessage={validationMessage}>
       <RichTextEditor
+        className={className}
         disabled={disabled}
         onChange={this.onChange}
-        placeholder={placeholder}
         value={this.state.value}
         webDriverTestID={id}
       />
-    </FormGroup >
+    </FormGroup>
   }
 }
 
-TextAreaFormGroup.defaultProps = {
-  disabled: false,
-  placeholder: '',
-  required: false
-}
 
-TextAreaFormGroup.propTypes = {
-  disabled: PropTypes.bool,
-  error: PropTypes.string,
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-  required: PropTypes.bool,
-  value: PropTypes.string
-}
