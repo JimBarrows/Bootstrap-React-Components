22998c43791c7d86d7b5f14d1a41b71ebeb56dc0
Jim Barrows <JIm.Barrows@gmail.com>
2018-07-01 08:58:05 -0500

    Media and others added.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,48 @@
 import Alert from './bootstrap/components/Alert'
 import Badge from './bootstrap/components/Badge'
+import BadgePill from './bootstrap/components/BadgePill'
+import Breadcrumb from './bootstrap/components/Breadcrumb'
+import BreadcrumbItem from './bootstrap/components/BreadcrumbItem'
+import Button from './bootstrap/components/Buitton'
 import ButtonGroup from './bootstrap/components/ButtonGroup'
+import ButtonToolbar from './bootstrap/components/ButtonToolbar'
+import Card from './bootstrap/components/Card'
+import CardBody from './bootstrap/components/CardBodyy'
+import CardColumn from './bootstrap/components/CardColumn'
+import CardDeck from './bootstrap/components/CardDeck'
+import CardFooter from './bootstrap/components/CardFooter'
+import CardGroup from './bootstrap/components/CardGroup'
+import CardHeader from './bootstrap/components/CardHeader'
+import CardImage from './bootstrap/components/CardImage'
+import CardImageOverlay from './bootstrap/components/CardImageOverlay'
+import CardTabs from './bootstrap/components/CardTabs'
+import CardText from './bootstrap/components/CardText'
+import CardTitle from './bootstrap/components/CardTitle'
+import Jumbotron from './bootstrap/components/Jumbotron'
 import ListGroup from './bootstrap/components/ListGroup/index'
-import Index from './bootstrap/components/Pagination/index'
+import ListGroupItem from './bootstrap/components/ListGroup/ListGroupItem'
+import Nav from './bootstrap/components/Nav/index'
+import NavItem from './bootstrap/components/Nav/NavItem'
+import NavbarBrand from './bootstrap/components/Navbar/Brand'
+import Navbar from './bootstrap/components/Navbar/index'
+import NavbarItem from './bootstrap/components/Navbar/Item'
+import Pagination from './bootstrap/components/Pagination/index'
+import PaginationItem from './bootstrap/components/Pagination/PaginationItem'
 import Progress from './bootstrap/components/Progress'
 import ProgressBar from './bootstrap/components/ProgressBar'
-import ProgressBarSingle from './bootstrap/components/ProgressBarSingle'
+import FormCheck from './bootstrap/forms/FormCheck'
+import FormControl from './bootstrap/forms/FormControl'
 import FormGroup from './bootstrap/forms/FormGroup'
+import TextArea from './bootstrap/forms/TextArea'
+import AppendAddon from './bootstrap/inputGroup/AppendAddon'
+import InputGroup from './bootstrap/inputGroup/index'
+import PrependAddon from './bootstrap/inputGroup/PrependAddon'
 import Media from './bootstrap/media/Media'
 import MediaBody from './bootstrap/media/MediaBody'
+import MediaHeading from './bootstrap/media/MediaHeading'
 import MediaLeft from './bootstrap/media/MediaLeft'
+import MediaList from './bootstrap/media/MediaList'
+import MediaListItem from './bootstrap/media/MediaListItem'
 import MediaObjectImage from './bootstrap/media/MediaObjectImage'
 import MediaRight from './bootstrap/media/MediaRight'
 import ListInline from './bootstrap/typography/ListInline'
@@ -20,12 +53,11 @@ import ReloadButton from './buttons/ReloadButton'
 import RemoveButton from './buttons/RemoveButton'
 import RowControlButtons from './buttons/RowControlButtons'
 import SaveButton from './buttons/SaveButton'
-import EditableTextCell from './table/EditableTextCell'
 import EditableListGroup from './EditableListGroup'
 import CheckboxFormGroup from './formgroups/CheckboxFormGroup'
 import ColorFormGroup from './formgroups/ColorFormGroup'
-import DateTimeLocalFormGroup from './formgroups/DateTimeLocalFormGroup'
 import DateRangeFormGroup from './formgroups/DateRangeFormGroup'
+import DateTimeLocalFormGroup from './formgroups/DateTimeLocalFormGroup'
 import EmailFormGroup from './formgroups/EmailFormGroup'
 import FileFormGroup from './formgroups/FileFormGroup'
 import NumberFormGroup from './formgroups/NumberFormGroup'
@@ -38,18 +70,67 @@ import TextAreaFormGroup from './formgroups/TextAreaFormGroup'
 import TextFormGroup from './formgroups/TextFormGroup'
 import UrlFormGroup from './formgroups/UrlFormGroup'
 import PageHeader from './PageHeader'
-import PanelStripedTable from './panels/PanelStripedTable'
 import Panel from './panels/Panel'
 import PanelBody from './panels/PanelBody'
 import PanelFooter from './panels/PanelFooter'
 import PanelHeader from './panels/PanelHeader'
+import PanelStripedTable from './panels/PanelStripedTable'
 import PanelWithAddButton from './panels/PanelWithAddButton'
+import EditableTextCell from './table/EditableTextCell'
 
 module.exports = {
-  AddButton,
   Alert,
   Badge,
+  BadgePill,
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
   ButtonGroup,
+  ButtonToolbar,
+  Card,
+  CardBody,
+  CardColumn,
+  CardDeck,
+  CardFooter,
+  CardGroup,
+  CardHeader,
+  CardImage,
+  CardImageOverlay,
+  CardTabs,
+  CardText,
+  CardTitle,
+  Jumbotron,
+  ListGroup,
+  ListGroupItem,
+  Nav,
+  NavItem,
+  Navbar,
+  NavbarBrand,
+  NavbarItem,
+  Pagination,
+  PaginationItem,
+  Progress,
+  ProgressBar,
+  FormCheck,
+  FormControl,
+  FormGroup,
+  TextArea,
+  InputGroup,
+  AppendAddon,
+  PrependAddon,
+  Media,
+  MediaBody,
+  MediaHeading,
+  MediaLeft,
+  MediaObjectImage,
+  MediaRight,
+  MediaList,
+  MediaListItem,
+  MediaObjectImage,
+  MediaRight,
+
+
+  AddButton,
   CancelButton,
   CheckboxFormGroup,
   ColorFormGroup,
@@ -61,26 +142,16 @@ module.exports = {
   EmailFormGroup,
   FileFormGroup,
   FormGroup,
-  ListGroup,
   ListInline,
   ListTablePanel     : PanelStripedTable,
-  Media,
-  MediaBody,
-  MediaLeft,
-  MediaObjectImage,
-  MediaRight,
   NumberFormGroup,
   PageHeader,
-  Pagination         : Index,
   Panel,
   PanelBody,
   PanelFooter,
   PanelHeader,
   PanelWithAddButton,
   PasswordFormGroup,
-  Progress,
-  ProgressBar,
-  ProgressBarSingle,
   RangeFormGroup,
   ReloadButton,
   RemoveButton,
