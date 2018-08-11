b29f69d601b77f9a1b2491075fca4972b626ad9c
Jim Barrows <JIm.Barrows@gmail.com>
2018-08-11 09:43:13 -0500

    [#117] Refer to github issue fixes #117

--- src/formgroups/NumberFormGroup.jsx
+++ src/formgroups/NumberFormGroup.jsx
@@ -4,28 +4,27 @@ import FormControl from '../bootstrap/forms/FormControl'
 import FormGroup from '../bootstrap/forms/FormGroup'
 
 export default class NumberFormGroup extends React.Component {
-  static defaultProps = {}
+	static defaultProps = {}
 
-  static propTypes = {
-    disabled         : PropTypes.bool,
-    id               : PropTypes.string.isRequired,
-    label            : PropTypes.string,
-    onChange         : PropTypes.func.isRequired,
-    required         : PropTypes.bool,
-    value            : PropTypes.number.isRequired,
-    valid            : PropTypes.bool,
-    validationMessage: PropTypes.string
-  }
+	static propTypes = {
+		disabled         : PropTypes.bool,
+		id               : PropTypes.string.isRequired,
+		label            : PropTypes.string,
+		onChange         : PropTypes.func.isRequired,
+		required         : PropTypes.bool,
+		value            : PropTypes.number.isRequired,
+		valid            : PropTypes.bool,
+		validationMessage: PropTypes.string
+	}
 
-  render() {
-    let {disabled, id, label, onChange, required, value, valid, validationMessage} = this.props
-    let componentId = `NumberFormGroup-${id}`
-    return (
-      <FormGroup id={componentId} label={label} required={required} valid={valid}
-                 validationMessage={validationMessage}>
-        <FormControl disabled={disabled} id={componentId} onChange={onChange} type={'number'} valid={valid}
-                     validated={validationMessage} value={value}/>
-      </FormGroup>
-    )
-  }
+	render() {
+		let {disabled, id, label, onChange, required, value, valid, validationMessage} = this.props
+		let componentId                                                                = `NumberFormGroup-${id}`
+		return (
+			<FormGroup id={componentId} label={label} required={required} valid={valid} validationMessage={validationMessage}>
+				<FormControl disabled={disabled} id={componentId} onChange={onChange} type={'number'} valid={valid}
+				             validated={validationMessage} value={value.toString()}/>
+			</FormGroup>
+		)
+	}
 }
