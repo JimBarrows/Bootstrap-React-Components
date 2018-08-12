52d2065cf0fa3d9addae4b1985bb1501712a1088
Jim Barrows <JIm.Barrows@gmail.com>
2018-08-12 07:50:36 -0500

    [#120] Refer to github issue fixes #120

--- src/formgroups/TextAreaFormGroup.jsx
+++ src/formgroups/TextAreaFormGroup.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
 import FormGroup from '../bootstrap/forms/FormGroup'
 
 export default class TextAreaFormGroup extends React.Component {
-	static defaultProps = {}
 
 	static propTypes = {
 		disabled         : PropTypes.bool,
@@ -17,32 +15,26 @@ export default class TextAreaFormGroup extends React.Component {
 		valid            : PropTypes.bool,
 		validationMessage: PropTypes.string
 	}
-	       onChange  = value => {
-		       this.setState({value})
-		       if (this.props.onChange) {
-			       this.props.onChange(
-				       {
-					       target:
-						       {
-							       id   : this.props.id,
-							       value: value
-						       }
-				       }
-			       )
-		       }
-	       }
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			value: props.value
+	onChange = value => {
+		if (this.props.onChange) {
+			this.props.onChange(
+				{
+					target:
+						{
+							id   : this.props.id,
+							value: value
+						}
+				}
+			)
 		}
 	}
 
+
 	render() {
-		let {disabled, id, label, required, valid, validationMessage} = this.props
-		let componentId                                               = `TextAreaFormGroup-${id}`
-		let className                                                 = 'form-control'
+		let {id, label, required, valid, validationMessage, value} = this.props
+		let componentId                                            = `TextAreaFormGroup-${id}`
+		let className                                              = 'form-control'
 		if (validationMessage) {
 			className += valid ? " is-valid" : " is-invalid"
 		}
@@ -52,7 +44,7 @@ export default class TextAreaFormGroup extends React.Component {
 				id={componentId}
 				className={className}
 				onChange={this.onChange}
-				value={this.state.value}
+				value={value}
 			/>
 		</FormGroup>
 	}
