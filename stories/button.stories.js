8bbcda5f578a56ff35f46554fc621b58d30e0376
Jim Barrows <Jim.Barrows@gmail.com>
2024-02-09 09:48:18 -0600

    changes

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,82 +1,84 @@
-import {action} from '@storybook/addon-actions'
+import {action}    from '@storybook/addon-actions'
 import {storiesOf} from '@storybook/react'
 
-import React from 'react'
-import Button from '../src/bootstrap/components/Button'
-import AddButton from '../src/buttons/AddButton'
-import CancelButton from '../src/buttons/CancelButton'
-import EditButton from '../src/buttons/EditButton'
-import ReloadButton from '../src/buttons/ReloadButton'
-import RemoveButton from '../src/buttons/RemoveButton'
+import React             from 'react'
+import Button            from '../src/bootstrap/components/Button'
+import AddButton         from '../src/buttons/AddButton'
+import CancelButton      from '../src/buttons/CancelButton'
+import EditButton        from '../src/buttons/EditButton'
+import ReloadButton      from '../src/buttons/ReloadButton'
+import RemoveButton      from '../src/buttons/RemoveButton'
 import RowControlButtons from '../src/buttons/RowControlButtons'
-import SaveButton from '../src/buttons/SaveButton'
+import SaveButton        from '../src/buttons/SaveButton'
 
 
 storiesOf('bootstrap/components/Button', module)
-  .addDecorator((story) =>
-    <div className="container">
-      {story()}
-    </div>)
-  .add('Default', () => <Button id={'default'}>Press me</Button>)
-  .add('Danger', () => <Button id={'danger'} onClick={action('Danger Button Pressed')} context={'danger'}>Press
-                                                                                                          me</Button>)
-  .add('light', () => <Button id={'light'} onClick={action('Light Button Pressed')} context={'light'}>Press me</Button>)
-  .add('Dark', () => <Button id={'dark'} onClick={action('Dark Button Pressed')} context={'dark'}>Press me</Button>)
-  .add('Info', () => <Button id={'info'} onClick={action('Info Button Pressed')} context={'info'}>Press me</Button>)
-  .add('Link', () => <Button id={'link'} onClick={action('Link Button Pressed')} context={'link'}>Press me</Button>)
-  .add('Primary', () => <Button id={'primary'} onClick={action('Primary Button Pressed')} context={'primary'}>Press
-                                                                                                              me</Button>)
-  .add('Secondary', () => <Button id={'secondary'} onClick={action('Secondary Button Pressed')} context={'secondary'}>Press
-                                                                                                                      me</Button>)
-  .add('Success', () => <Button id={'success'} onClick={action('Success Button Pressed')} context={'success'}>Press
-                                                                                                              me</Button>)
-  .add('Warning', () => <Button id={'warning'} onClick={action('Warning Button Pressed')} context={'warning'}>Press
-                                                                                                              me</Button>)
-  .add('Outline Danger', () => <Button id={'danger'} onClick={action('Danger Button Pressed')} outline={true}
-                                       context={'danger'}>Press me</Button>)
-  .add('Outline Dark', () => <Button id={'dark'} onClick={action('Dark Button Pressed')} outline={true}
-                                     context={'dark'}>Press
-                                                      me</Button>)
-  .add('Outline Info', () => <Button id={'info'} onClick={action('Info Button Pressed')} outline={true}
-                                     context={'info'}>Press
-                                                      me</Button>)
-  .add('Outline Primary', () => <Button id={'primary'} onClick={action('Primary Button Pressed')} outline={true}
-                                        context={'primary'}>Press me</Button>)
-  .add('Outline Secondary', () => <Button id={'secondary'} onClick={action('Secondary Button Pressed')} outline={true}
-                                          context={'secondary'}>Press me</Button>)
-  .add('Outline Success', () => <Button id={'success'} onClick={action('Success Button Pressed')} outline={true}
-                                        context={'success'}>Press me</Button>)
-  .add('Outline Warning', () => <Button id={'warning'} onClick={action('Warning Button Pressed')} outline={true}
-                                        context={'warning'}>Press me</Button>)
-  .add('Large', () => <Button id={'large'} onClick={action('Large Button Pressed')} size={'large'} context={'info'}>Press
-                                                                                                                    me</Button>)
-  .add('Small', () => <Button id={'small'} onClick={action('Small Button Pressed')} size={'small'} context={'info'}>Press
-                                                                                                                    me</Button>)
-  .add('Block', () => <Button block={true} id={'block'} onClick={action('Block Button Pressed')} context={'info'}>Press
-                                                                                                                  me</Button>)
-  .add('Disabled', () => <Button disabled={true} id={'block'} onClick={action('Block Button Pressed')} context={'info'}>Press
-                                                                                                                        me</Button>)
+	.addDecorator((story) =>
+		<div className="container" >
+			{story()}
+		</div >)
+	.add('Default', () => <Button id={'default'} >Press me</Button >)
+	.add('Danger', () => <Button id={'danger'} onClick={action('Danger Button Pressed')} context={'danger'} >Press
+		me</Button >)
+	.add('light', () => <Button id={'light'} onClick={action('Light Button Pressed')} context={'light'} >Press
+		me</Button >)
+	.add('Dark', () => <Button id={'dark'} onClick={action('Dark Button Pressed')} context={'dark'} >Press me</Button >)
+	.add('Info', () => <Button id={'info'} onClick={action('Info Button Pressed')} context={'info'} >Press me</Button >)
+	.add('Link', () => <Button id={'link'} onClick={action('Link Button Pressed')} context={'link'} >Press me</Button >)
+	.add('Primary', () => <Button id={'primary'} onClick={action('Primary Button Pressed')} context={'primary'} >Press
+		me</Button >)
+	.add('Secondary', () => <Button id={'secondary'} onClick={action('Secondary Button Pressed')} context={'secondary'} >Press
+		me</Button >)
+	.add('Success', () => <Button id={'success'} onClick={action('Success Button Pressed')} context={'success'} >Press
+		me</Button >)
+	.add('Warning', () => <Button id={'warning'} onClick={action('Warning Button Pressed')} context={'warning'} >Press
+		me</Button >)
+	.add('Outline Danger', () => <Button id={'danger'} onClick={action('Danger Button Pressed')} outline={true}
+		context={'danger'} >Press me</Button >)
+	.add('Outline Dark', () => <Button id={'dark'} onClick={action('Dark Button Pressed')} outline={true}
+		context={'dark'} >Press
+		me</Button >)
+	.add('Outline Info', () => <Button id={'info'} onClick={action('Info Button Pressed')} outline={true}
+		context={'info'} >Press
+		me</Button >)
+	.add('Outline Primary', () => <Button id={'primary'} onClick={action('Primary Button Pressed')} outline={true}
+		context={'primary'} >Press me</Button >)
+	.add('Outline Secondary', () => <Button id={'secondary'} onClick={action('Secondary Button Pressed')} outline={true}
+		context={'secondary'} >Press me</Button >)
+	.add('Outline Success', () => <Button id={'success'} onClick={action('Success Button Pressed')} outline={true}
+		context={'success'} >Press me</Button >)
+	.add('Outline Warning', () => <Button id={'warning'} onClick={action('Warning Button Pressed')} outline={true}
+		context={'warning'} >Press me</Button >)
+	.add('Large', () => <Button id={'large'} onClick={action('Large Button Pressed')} size={'large'} context={'info'} >Press
+		me</Button >)
+	.add('Small', () => <Button id={'small'} onClick={action('Small Button Pressed')} size={'small'} context={'info'} >Press
+		me</Button >)
+	.add('Block', () => <Button block={true} id={'block'} onClick={action('Block Button Pressed')} context={'info'} >Press
+		me</Button >)
+	.add('Disabled', () => <Button disabled={true} id={'block'} onClick={action('Block Button Pressed')}
+		context={'info'} >Press
+		me</Button >)
 
 
 storiesOf('buttons', module)
-  .addDecorator((story) =>
-    <div className="container">
-      {story()}
-    </div>)
-  .add('Add Button', () => <AddButton id={'add'} onClick={action('Add Button pressed')}/>)
-  .add('Cancel Button', () => <CancelButton id={'cancel'} onClick={action('Cancel Button pressed')}/>)
-  .add('Edit Button', () => <EditButton id={'edit'} onClick={action('Edit Button pressed')}/>)
-  .add('Reload Button', () => <ReloadButton id={'reload'} onClick={action('Reload Button pressed')}/>)
-  .add('Remove Button', () => <RemoveButton id={'remove'} onClick={action('Remove Button pressed')}/>)
-  .add('Row Control Buttons Not Editing', () => <RowControlButtons id={'row-controls'}
-                                                                   cancel={action('Cancel Button pressed')}
-                                                                   edit={action('Edit button pressed')}
-                                                                   save={action('Save button pressed')}
-                                                                   remove={action('Remove button pressed')}/>)
-  .add('Row Control Buttons Editing', () => <RowControlButtons id={'row-controls'}
-                                                               cancel={action('Cancel Button pressed')}
-                                                               edit={action('Edit button pressed')}
-                                                               editing={true}
-                                                               save={action('Save button pressed')}
-                                                               remove={action('Remove button pressed')}/>)
-  .add('Save Button', () => <SaveButton id={'remove'} onClick={action('Save Button pressed')}/>)
+	.addDecorator((story) =>
+		<div className="container" >
+			{story()}
+		</div >)
+	.add('Add Button', () => <AddButton id={'add'} onClick={action('Add Button pressed')} />)
+	.add('Cancel Button', () => <CancelButton id={'cancel'} onClick={action('Cancel Button pressed')} />)
+	.add('Edit Button', () => <EditButton id={'edit'} onClick={action('Edit Button pressed')} />)
+	.add('Reload Button', () => <ReloadButton id={'reload'} onClick={action('Reload Button pressed')} />)
+	.add('Remove Button', () => <RemoveButton id={'remove'} onClick={action('Remove Button pressed')} />)
+	.add('Row Control Buttons Not Editing', () => <RowControlButtons id={'row-controls'}
+		cancel={action('Cancel Button pressed')}
+		edit={action('Edit button pressed')}
+		save={action('Save button pressed')}
+		remove={action('Remove button pressed')} />)
+	.add('Row Control Buttons Editing', () => <RowControlButtons id={'row-controls'}
+		cancel={action('Cancel Button pressed')}
+		edit={action('Edit button pressed')}
+		editing={true}
+		save={action('Save button pressed')}
+		remove={action('Remove button pressed')} />)
+	.add('Save Button', () => <SaveButton id={'remove'} onClick={action('Save Button pressed')} />)
--- stories/modal.stories.js
+++ stories/modal.stories.js
@@ -6,7 +6,6 @@ import ModalBody   from '../src/bootstrap/components/modal/Body'
 import ModalFooter from '../src/bootstrap/components/modal/Footer'
 import ModalHeader from '../src/bootstrap/components/modal/Header'
 
-
 storiesOf('bootstrap/components/Modal', module)
 	.addDecorator((story) => <div className="container" >       {story()}     </div >)
 	.add('Basic', () => <Modal id={'basic'} show={true} >
@@ -19,22 +18,13 @@ storiesOf('bootstrap/components/Modal', module)
 			<button type="button" className="btn btn-primary" >Save changes</button >
 		</ModalFooter >
 	</Modal >)
-	.add('bootstrap', () => <div className="modal show" tabIndex="-1" role="dialog" >
-		<div className="modal-dialog" >
-			<div className="modal-content" >
-				<div className="modal-header" >
-					<h5 className="modal-title" >Modal title</h5 >
-					<button type="button" className="close" data-dismiss="modal" aria-label="Close" >
-						<span aria-hidden="true" >&times;</span >
-					</button >
-				</div >
-				<div className="modal-body" >
-					<p >Modal body text goes here.</p >
-				</div >
-				<div className="modal-footer" >
-					<button type="button" className="btn btn-secondary" data-dismiss="modal" >Close</button >
-					<button type="button" className="btn btn-primary" >Save changes</button >
-				</div >
-			</div >
-		</div >
-	</div >)
+	.add('Button Activated', () => <Modal id={'basic'} show={true} >
+		<ModalHeader id={'basic'} title={'Modal title'} />
+		<ModalBody id={'basic'} >
+			<p >Modal body text goes here.</p >
+		</ModalBody >
+		<ModalFooter id={'basic'} >
+			<button type="button" className="btn btn-secondary" data-dismiss="modal" >Close</button >
+			<button type="button" className="btn btn-primary" >Save changes</button >
+		</ModalFooter >
+	</Modal >)
